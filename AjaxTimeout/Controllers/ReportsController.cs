3a3e9eacfa467700bdd28c273183d2e96832fa69
joe dillon <joe912@gmail.com>
2013-10-15 12:09:36 +0100

    initial commit - see ReportsController

---
+++ AjaxTimeout/Controllers/ReportsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxTimeout.Controllers
+{
+
+    public class ReportsController : BaseController
+    {
+        //
+        // GET: /Reports/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SummaryExpansion(string blah)
+        {
+            if (blah == "throw")
+            {
+                throw new ApplicationException("an unexpected error occurred - pls try again");
+            }
+            
+            System.Threading.Thread.Sleep(5000);
+
+            return PartialView("SummaryGroupExpansion");
+        }
+    }
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
---
+++ AjaxTimeout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxTimeout.MvcApplication" Language="C#" %>
